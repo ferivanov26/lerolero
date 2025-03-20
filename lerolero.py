f886b0ba9f177cf8add55cfe5d27d97e5f246ab9
Fernando Ivanov <fernando.ivanov@usp.br>
2025-03-20 10:53:22 -0300

    Salvei as frases

--- lerolero.py
+++ lerolero.py
@@ -5,11 +5,29 @@ Gera frases de efeito sem significado real."""
 
 import random
 
-parte1=[]
-parte2=[]
-parte3=[]
+parte1=[
+  "O sistema em desenvolvimento",
+  "O novo protocolo de comunicação",
+  "O algoritmo otimizado"
 
-print (random.choice(parte1), random.choice(parte2), random(choice(parte3))
+]
+
+
+parte2=[
+  "possui excelente desempenho",
+  "oferece garantias de precisão acima da média",
+  "preenche uma lacuna significativa"
+
+]
+
+
+parte3=[
+  "nas aplicações a que se destina",
+  "em relação às opcoes disponiveis no mercado",
+  ", provendo ampla vantagem competitiva a seus usuarios" 
+]
+
+print (random.choice(parte1), random.choice(parte2), random(choice(parte3), leobahiaehmuitosigma)
 
 
 
