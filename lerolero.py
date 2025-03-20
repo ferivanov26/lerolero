eb0dddaae58e8aef8621fe8b462e30bb5b1f8191
leomoura <mourafigueiredoleo@gmail.com>
2025-03-20 10:45:56 -0300

    Cria esqueleto da versão multi-linguas

--- lerolero.py
+++ lerolero.py
@@ -8,8 +8,14 @@ import random
 parte1=[]
 parte2=[]
 parte3=[]
-
+lingua = (int(input("Escolha a língua : 1 - português; 2 - inglês\n")))
+if(lingua == 2):
+	parte1 = []
+	parte2 = []
+	parte3 = []
 print (random.choice(parte1), random.choice(parte2), random(choice(parte3))
+
+
 
 
 
