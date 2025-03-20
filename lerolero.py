5d6d30fec3d9963c30d870bf89a00eac93981499
Fernando Ivanov <fernando.ivanov@usp.br>
2025-03-20 10:33:29 -0300

    Implenta o esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -3,4 +3,13 @@
 
 Gera frases de efeito sem significado real."""
 
+import random
+
+parte1=[]
+parte2=[]
+parte3=[]
+
+print (random.choice(parte1), random.choice(parte2), random(choice(parte3))
+
+
 
